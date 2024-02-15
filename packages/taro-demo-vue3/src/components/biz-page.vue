b9c9c37e70dee016d5f6e33d0e86efeb5fcef881
anyesu <iewgggg@qq.com>
2024-02-15 17:28:00 +0800

    feat(vue3): :sparkles: add `biz-page`

---
+++ packages/taro-demo-vue3/src/components/biz-page.vue
@@ -0,0 +1,29 @@
+<template>
+  <nut-config-provider class="layout-screen" :theme-vars>
+    <view class="layout-content">
+      <slot />
+    </view>
+  </nut-config-provider>
+</template>
+
+<script setup lang="ts">
+const themeVars = ref({
+  primaryColor: '#a681fd',
+});
+</script>
+
+<style lang="scss">
+.layout-screen {
+  width: 100vw;
+  height: 100vh;
+  overflow-x: hidden;
+  display: flex;
+  flex-direction: column;
+
+  .layout-content {
+    height: 100%;
+    background: #f7f8fa;
+    padding: 30px 25px;
+  }
+}
+</style>
